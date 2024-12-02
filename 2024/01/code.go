3d4e1becf9ef57b96aa557f4287c5920fed6e935
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2024-12-02 04:19:14 +0000

    use map to count number appearance

--- 2024/01/code.go
+++ 2024/01/code.go
@@ -20,7 +20,6 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
-	// TODO: use Map for storing the values
 	var leftCol []string
 	var rightCol []string
 	var distance int = 0
@@ -29,15 +28,15 @@ func run(part2 bool, input string) any {
 		temp := strings.SplitN(line, "   ", 2)
 		leftCol = append(leftCol, temp[0])
 		rightCol = append(rightCol, temp[1])
-		// right := strings.SplitAfter(line, "   ")s
-
 	}
+
 	sort.Slice(leftCol, func(i, j int) bool {
 		return leftCol[i] < leftCol[j]
 	})
 	sort.Slice(rightCol, func(i, j int) bool {
 		return rightCol[i] < rightCol[j]
 	})
+
 	for i := 0; i < len(leftCol); i++ {
 		leftInt, _ := strconv.Atoi(leftCol[i])
 		rightInt, _ := strconv.Atoi(rightCol[i])
@@ -45,26 +44,19 @@ func run(part2 bool, input string) any {
 	}
 
 	if part2 {
-		/* Calculate a total similarity score by adding up each number
-		in the left list after multiplying it by the number of times
-		that number appears in the right list.
-		*/
 		var score int = 0
-		var rightCount int = 0
-		for i := 0; i < len(leftCol); i++ {
-			leftInt, _ := strconv.Atoi(leftCol[i])
-			rightCount = 0
-			for j := 0; j < len(rightCol); j++ {
-				rightInt, _ := strconv.Atoi(rightCol[j])
-				if leftInt == rightInt {
-					rightCount++
-				}
-			}
-			score += leftInt * rightCount
+		rightCountMap := make(map[int]int)
+		for _, val := range rightCol {
+			rightInt, _ := strconv.Atoi(val)
+			rightCountMap[rightInt]++
+		}
+
+		for _, val := range leftCol {
+			leftInt, _ := strconv.Atoi(val)
+			score += leftInt * rightCountMap[leftInt]
 		}
 
 		return score
 	}
-	// solve part 1 here
 	return distance
 }
