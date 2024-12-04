fdb234977763bb5e85989aa9de32cd8303779dbb
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2024-12-04 04:10:19 +0000

    simplify day3 part2

--- 2024/03/code.go
+++ 2024/03/code.go
@@ -14,51 +14,47 @@ func main() {
 
 func run(part2 bool, input string) any {
 	//Part 1 regex
+	result := 0
 	regex, err := regexp.Compile(`mul[(][0-9]{1,3},[0-9]{1,3}[)]`)
 	if err != nil {
 		log.Fatal(err)
 	}
 	commands := regex.FindAllString(input, -1)
-	result := calculateResult(commands)
-	removed := 0
+	for _, command := range commands {
+		result += calculateResult(command)
+	}
+
 	if part2 {
 		part2Result := 0
-		newInput := input
-		// find all string between don't() and do()
-		part2Regex, err := regexp.Compile(`don't\(\)(.*?)do\(\)`)
+		part2Regex, err := regexp.Compile(`don't\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)`)
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		isEnabled := true
 		for _, part2RegexCommands := range part2Regex.FindAllString(input, -1) {
-			removedCommands := regex.FindAllString(part2RegexCommands, -1)
-			if len(removedCommands) > 0 {
-				removed += calculateResult(removedCommands)
+
+			if part2RegexCommands == "don't()" {
+				isEnabled = false
+			} else if part2RegexCommands == "do()" {
+				isEnabled = true
+			} else if isEnabled {
+
+				part2Result += calculateResult(part2RegexCommands)
 			}
-			//newInput = strings.ReplaceAll(newInput, part2RegexCommands, "")
-
 		}
-		println("re:", removed)
-
-		newCommands := regex.FindAllString(newInput, -1)
-		println(187194524 - removed)
-		part2Result = calculateResult(newCommands)
 		return part2Result
 	}
 	return result
 }
 
-func calculateResult(commands []string) int {
+func calculateResult(command string) int {
 	result := 0
 	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-	for _, command := range commands {
-		// println(command)
-		matches := re.FindStringSubmatch(command)
-		if len(matches) == 3 {
-			x, _ := strconv.Atoi(matches[1])
-			y, _ := strconv.Atoi(matches[2])
-			result += x * y
-		}
+	matches := re.FindStringSubmatch(command)
+	if len(matches) == 3 {
+		x, _ := strconv.Atoi(matches[1])
+		y, _ := strconv.Atoi(matches[2])
+		result += x * y
 	}
 	return result
 }
