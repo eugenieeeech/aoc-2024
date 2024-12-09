cf26facc71e55eac511a1ab3843b7d749abf45ed
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2024-12-09 05:06:48 +0000

    clean up

--- 2024/07/code.go
+++ 2024/07/code.go
@@ -21,13 +21,15 @@ func run(part2 bool, input string) any {
 			continue
 		}
 		numbers := strings.Split(strings.TrimSpace(temp[1]), " ")
-		if match(leftInt, numbers, 0, 0, true) || match(leftInt, numbers, 0, 0, false) {
-			sum += leftInt
+		if part2 {
+			if match2(leftInt, numbers, 0, 0, true, true) || match2(leftInt, numbers, 0, 0, false, true) || match2(leftInt, numbers, 0, 0, true, false) || match2(leftInt, numbers, 0, 0, false, false) {
+				sum += leftInt
+			}
+		} else {
+			if match(leftInt, numbers, 0, 0, true) || match(leftInt, numbers, 0, 0, false) {
+				sum += leftInt
+			}
 		}
-	}
-
-	if part2 {
-		return "not implemented"
 	}
 	return sum
 }
@@ -41,4 +43,20 @@ func match(target int, numbers []string, index int, current int, add bool) bool 
 		return match(target, numbers, index+1, current+num, true) || match(target, numbers, index+1, current+num, false)
 	}
 	return match(target, numbers, index+1, current*num, true) || match(target, numbers, index+1, current*num, false)
+}
+
+func match2(target int, numbers []string, index int, current int, add bool, concatenation bool) bool {
+	if index == len(numbers) {
+		return current == target
+	}
+	num, _ := strconv.Atoi(numbers[index])
+	if add {
+		return match2(target, numbers, index+1, current+num, true, true) || match2(target, numbers, index+1, current+num, false, false) || match2(target, numbers, index+1, current+num, true, false) || match2(target, numbers, index+1, current+num, false, true)
+	}
+	if concatenation {
+		concatenatedNum, _ := strconv.Atoi(strconv.Itoa(current) + numbers[index])
+		return match2(target, numbers, index+1, concatenatedNum, true, true) || match2(target, numbers, index+1, concatenatedNum, false, false) || match2(target, numbers, index+1, concatenatedNum, true, false) || match2(target, numbers, index+1, concatenatedNum, false, true)
+	}
+	return match2(target, numbers, index+1, current*num, true, true) || match2(target, numbers, index+1, current*num, false, false) || match2(target, numbers, index+1, current*num, true, false) || match2(target, numbers, index+1, current*num, false, true)
+
 }
